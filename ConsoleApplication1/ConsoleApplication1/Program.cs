b00d15a5a9c6a86fcf164bbf1cc3bc8cba731ba2
Fake Haack <haacked+github@gmail.com>
2016-04-22 12:57:08 -0500

    Whoops

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.Write("HOLA PERU!");
+            xConsole.Write("HOLA PERU!");
         }
     }
 }
