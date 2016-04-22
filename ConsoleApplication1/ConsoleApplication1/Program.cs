c7cd36c295ff614c76aac20e242210f4d2066cf2
Fake Haack <haacked+github@gmail.com>
2016-04-22 13:00:41 -0500

    test

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            xConsole.Write("HOLA PERU!");
+           x xConsole.Write("HOLA PERU!");
         }
     }
 }
